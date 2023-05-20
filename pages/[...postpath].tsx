52d81ffba23c07ec8d9bb5a7289e8dc4300986cf
SurajRajput090 <133194411+SurajRajput090@users.noreply.github.com>
2023-05-20 14:29:59 +0545

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					` https://cryptoanalyticss.000webhostapp.com/` + encodeURI(path as string)
+					`https://www.cryptoanalyticss.online/` + encodeURI(path as string)
 				}`,
 			},
 		};
